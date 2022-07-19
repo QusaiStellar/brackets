90096289e5e6c25491435d3d948f997fb6cdb5dd
Andrew Dobrik <omgdobrik@gmail.com>
2022-07-19 11:25:10 +0300

    feat: so so

---
+++ src/index copy.js
@@ -0,0 +1,40 @@
+const config1 = [['(', ')']];
+const config2 = [['(', ')'], ['[', ']']];
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
+const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
+const str = '||';
+
+
+function check(str, bracketsConfig) {
+   let config = [];
+   let stack = [];
+
+   for (let i = 0; i < bracketsConfig.length; i++) {
+      config = config.concat([...bracketsConfig[i]]);
+   }
+
+   const brackets = config.join("");
+   console.log(brackets);
+
+   for (let bracket of str) {
+      let numberBracket = brackets.indexOf(bracket);
+      console.log(bracket);
+
+      if (numberBracket % 2 === 0) {
+         stack.push(numberBracket + 1);
+      } else {
+         if (numberBracket !== stack.pop()) {
+            return false;
+         }
+      }
+   }
+   console.log(stack);
+
+   return stack.length === 0;
+}
+
+console.log(check(str, config4));
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let config = [];
+   let stack = [];
+
+   for (let i = 0; i < bracketsConfig.length; i++) {
+      config = config.concat([...bracketsConfig[i]]);
+   }
+
+   const brackets = config.join("");
+
+   for (let bracket of str) {
+      let numberBracket = brackets.indexOf(bracket);
+      if (numberBracket % 2 === 0) {
+         stack.push(numberBracket + 1);
+      } else {
+         if (numberBracket !== stack.pop()) {
+            return false;
+         }
+      }
+   }
+
+   if (stack.length) {
+
+   }
+
+   return stack.length === 0;
 }
